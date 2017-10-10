d7f18b3f809e44676d67b6ba98a42fec8f98f4e3
Trevor Morse <mors6189@vandals.uidaho.edu>
2017-10-10 13:21:28 -0700

    Move function definitions to source files

--- hp_analysis/hp_analysis/pop.h
+++ hp_analysis/hp_analysis/pop.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "indiv.h"
 
 class pop {
 private:
@@ -14,78 +15,3 @@ public:
 	void nextGen();
 	int selectParent();
 };
-
-pop::pop() {
-	calcAvgs();
-}
-
-void pop::print() {
-	for (int i = 0; i < popSize; i++) {
-		the_pop[i].calcFit();
-		//printf("%d ",i);
-		//the_pop[i].print();
-	}
-	calcAvgs();
-	printf("Average Fitness: %f Best Fitness: %f  Best Individual %d genes:", avgFit, bestFit, best);
-	for (int g = 0; g < genomeLength; g++) {
-		printf(" %.3f", avgGenes[g]);
-	}
-	printf("\n");
-
-}
-
-void pop::calcAvgs() {
-	best = 0;
-	bestFit = the_pop[0].getFit();
-	avgFit = the_pop[0].getFit();
-	for (int i = 1; i < popSize; i++) {
-		avgFit += the_pop[i].getFit();
-		if (the_pop[i].getFit() > bestFit) {
-			best = i;
-			bestFit = the_pop[i].getFit();
-		}
-	}
-	avgFit /= popSize;
-
-	for (int g = 0; g < genomeLength; g++) {
-		avgGenes[g] = 0;
-		for (int i = 1; i < popSize; i++) {
-			avgGenes[g] += the_pop[i].getGene(g);
-		}
-		avgGenes[g] /= popSize;
-	}
-}
-
-void pop::nextGen() {
-	indiv temp[popSize];
-	temp[0].copy(the_pop[best]);  // elitism, make two copies of the best individual
-	temp[1].copy(the_pop[best]);
-	int p1, p2;
-	for (int i = 2; i < popSize; i += 2) {
-		p1 = selectParent();
-		p2 = selectParent();
-		temp[i].copy(the_pop[p1]);
-		temp[i + 1].copy(the_pop[p2]);
-		//temp[i].crossover(temp[i+1]);   // no crossover, because there's only one gene
-		temp[i].mutate();
-		temp[i + 1].mutate();
-	}
-	for (int i = 0; i < popSize; i++) {
-		the_pop[i].copy(temp[i]);
-	}
-}
-
-int pop::selectParent() {
-	int best, temp;
-	int bestFit;
-	best = rand() % popSize;
-	bestFit = the_pop[best].getFit();
-	for (int i = 1; i < tournSize; i++) {
-		temp = rand() % popSize;
-		if (the_pop[temp].getFit() > bestFit) {
-			best = temp;
-			bestFit = the_pop[best].getFit();
-		}
-	}
-	return best;
-}
