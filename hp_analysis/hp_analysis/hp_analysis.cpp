8d0004ea593ef94bc67005198f43ad0bc58d2a05
Trevor Morse <mors6189@vandals.uidaho.edu>
2017-10-10 13:16:30 -0700

    Initial files from Soule

---
+++ hp_analysis/hp_analysis/hp_analysis.cpp
@@ -0,0 +1,45 @@
+// hp_analysis.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+#include<cmath>
+#include<random>
+#include"indiv.h"
+#include"pop.h"
+using namespace std;
+
+void printHeader(time_t);
+
+int main() {
+	time_t theTime;
+	time(&theTime);
+	srand(theTime);
+	printHeader(theTime);  // record the time, which is the random seed, so experiments can be rerun.
+	pop p;
+	p.print();
+	for (int g = 0; g < 100; g++) {  // 100 generations
+		p.nextGen();
+		p.print();
+	}
+	return 0;
+}
+
+// prints information about the run into the data header
+void printHeader(time_t t) {
+	struct tm * timeinfo;
+	timeinfo = localtime(&t);
+	printf("Seed %d\n", t);
+	printf("%s", asctime(timeinfo));
+	printf("Attack type: %d\n", attackType);
+	printf("Fixed damage amount: %d\n", damageAmount);
+	printf("Mean damage: %f\n", meanDamage);
+	printf("Range of damage (+/- this amount): %f\n", rangeDamage);
+	printf("Mutation type: %d\n", mutateType);
+	printf("Mutation size: %d\n", uniformMutateRange);
+	printf("Pop size: %d\n", popSize);
+	printf("Tournament size: %d\n", tournSize);
+
+}
---
+++ hp_analysis/hp_analysis/params.h
@@ -0,0 +1,11 @@
+#pragma once
+const int genomeLength = 1;   // currently only one gene - health
+const int maxInitHealth = 10;  // maximum initial health
+const int mutateType = 0;  // 0-uniform
+const int uniformMutateRange = 6; // -5 to 5
+const int popSize = 100;
+const int attackType = 0;  // 0 - Constant Damage, 1 - Uniform Damage
+const int damageAmount = 15;  // Constant Damage amount
+const double meanDamage = 15;  // mean damage for uniform or Gaussian
+const double rangeDamage = 5;  // range of damage for uniform or Gaussian (+/- this amount)
+const int tournSize = 3;
