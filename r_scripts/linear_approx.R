2f6c3f7cb8826cf8dda0c2895262f1cf838daa0d
Trevor Morse <mors6189@vandals.uidaho.edu>
2017-11-09 12:44:21 -0800

    Adapt to handle a batch of csv files

--- r_scripts/linear_approx.R
+++ r_scripts/linear_approx.R
@@ -1,4 +1,12 @@
 #!/usr/bin/env Rscript
+
+# This script is used to find the linear approximation, and associated
+# values, from a batch of csv files. Each file is expected to have a
+# header of 9 lines listing info about the trial run, followed by
+# 100 rows of 3 columns of data.
+#
+# @author trevmo
+
 require(ggplot2)
 
 FindLinearApprox <- function (xvals, yvals, slopeLabel, r2Label) {
@@ -29,10 +37,13 @@ FindResults <- function(filename) {
   #
   # Returns:
   #   void (outputs results to a new file)
-  output <- paste("out", filename, sep="_")
-  header <- read.csv(args[1], nrows=9)
-  write.csv(header, file = output, row.names = FALSE)
-  dat <- read.csv(args[1], skip = 10)
+  dirName <- "stats"
+  dir.create(dirName, showWarnings = F)
+  output <- paste("stats", basename(filename), sep="_")
+  outputPath <- file.path(dirName, output)
+  header <- read.csv(filename, nrows=9)
+  write.csv(header, outputPath, row.names = FALSE)
+  dat <- read.csv(filename, skip = 10)
   gen <- dat$Generation
   avg <- dat$AvgFit
   best <- dat$BestFit
@@ -40,12 +51,15 @@ FindResults <- function(filename) {
   avgFinal <- FindLinearApprox(gen, avg, 'avgSlope', 'r^2')
   bestFinal <- FindLinearApprox(gen, best, 'bestSlope', 'r^2')
   #write the table out to the file created earlier
-  write.table(avgFinal, output, sep = ",", col.names = T, row.names = F, append = T)
-  write.table(bestFinal, output, sep = ",", col.names = T, row.names = F, append = T)
+  write.table(avgFinal, outputPath, sep = ",", col.names = T, row.names = F, append = T)
+  write.table(bestFinal, outputPath, sep = ",", col.names = T, row.names = F, append = T)
 }
 
 args = commandArgs(trailingOnly=TRUE)
-if (length(args) != 1) {
-  stop("You must specify a csv file on the command line.n")
+if (length(args) < 1) {
+  stop("You must specify csv file(s) on the command line.n")
 }
-FindResults(args[1])
+#loop over the csv files, creating output files
+for (arg in args) {
+  FindResults(arg)
+}
